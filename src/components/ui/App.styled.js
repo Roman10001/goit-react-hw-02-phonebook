56dc60721f4817f284b79e62b190bd693e3df0cc
Roman10001 <pavlenko.roman1@gmail.com>
2022-09-23 17:20:07 +0300

    save

---
+++ src/components/ui/App.styled.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+
+export const Container = styled.div`
+display: block;
+width: 700px;
+
+background-color: teal;
+
+margin: 20px auto 0 auto;
+padding: 20px;
+
+border-radius: 20px;
+box-shadow: 5px 4px 4px 2px rgba(10, 0, 0, 0.1);
+`
